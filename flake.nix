071bf01cdc64dd8999a49da89b7e82945909ecb9
RandomNEET <dev@randomneet.me>
2025-11-01 02:59:23 +0800

    switch to flake

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.systems.url = "github:nix-systems/default";
+  inputs.flake-utils = {
+    url = "github:numtide/flake-utils";
+    inputs.systems.follows = "systems";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        pythonEnv = pkgs.python313.withPackages (ps: [
+          ps.requests
+          ps.yt-dlp
+        ]);
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pythonEnv
+            pkgs.nodejs
+            pkgs.sqlite
+            pkgs.sqlitebrowser
+          ];
+        };
+      }
+    );
+}
