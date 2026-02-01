02a7876d0bd4a7756fb3983307297449a0a7ef31
RandomNEET <dev@randomneet.me>
2026-02-01 18:09:59 +0800

    update content

--- frontend/src/pages/Announcement.vue
+++ frontend/src/pages/Announcement.vue
@@ -16,6 +16,15 @@
 
         <div class="update-timeline">
           <div class="update-item">
+            <div class="update-date">2026.02.01</div>
+            <div class="update-content">
+              <ul>
+                <li>新收录作者6位，视频123个，熟肉110个</li>
+              </ul>
+            </div>
+          </div>
+
+          <div class="update-item">
             <div class="update-date">2026.01.25</div>
             <div class="update-content">
               <ul>
