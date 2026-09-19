9634064e7a0e35d4bb71b185799798ff1eb47014
RandomNEET <dev@randomneet.me>
2026-09-19 13:02:12 +0800

    weekly update

--- frontend/src/views/AnnounceView.vue
+++ frontend/src/views/AnnounceView.vue
@@ -16,6 +16,24 @@
 
         <div class="update-timeline">
           <div class="update-item">
+            <div class="update-date">2026.09.19</div>
+            <div class="update-content">
+              <ul>
+                <li>新收录作者4位，视频68个，熟肉66个</li>
+                <li>
+                  本次更新详情:
+                  <a
+                    href="https://www.dolthub.com/repositories/randomneet/random-2hu-stuff/compare/main/fdf5cqde6d05pbjqf1pdtcvbsfp775fq"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    >dolthub</a
+                  >
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div class="update-item">
             <div class="update-date">2026.09.13</div>
             <div class="update-content">
               <ul>
